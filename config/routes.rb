0b097ec414fc32ea5f683ef95a693c53834b1deb
Filippo Quattrocchi <filippo.quattrocchi23@outlook.it>
2024-08-19 11:47:47 +0900

    Feat: Crud operations for articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  get 'render/index'
+  # get 'render/index'
+  # get '/articles', to: "articles#index"
+  # get '/articles/:id', to: "articles#show"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
+  resources :articles
   # root "articles#index"
-  root "render#index"
+  root "articles#index"
 end
