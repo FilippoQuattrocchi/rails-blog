1e9a66138e1a9ce6cfe4c5d4e97dfe2dd9376635
Filippo Quattrocchi <filippo.quattrocchi23@outlook.it>
2024-08-22 13:48:59 +0900

    Migration for change comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_08_21_052341) do
+ActiveRecord::Schema[7.0].define(version: 2024_08_22_044627) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -62,13 +62,14 @@ ActiveRecord::Schema[7.0].define(version: 2024_08_21_052341) do
   end
 
   create_table "comments", force: :cascade do |t|
-    t.string "commenter"
     t.text "body"
     t.bigint "article_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status"
+    t.bigint "user_id", null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -85,4 +86,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_08_21_052341) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "articles"
+  add_foreign_key "comments", "users"
 end
